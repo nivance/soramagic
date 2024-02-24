9111362ece54fec96e1fe5a7d18973601ccba40f
nivance <limingjian110@163.com>
2024-02-24 18:59:14 +0800

    add content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{tsx,css}'],
+  content: ['./src/**/*.{js, ts, jsx, tsx, css, mdx}'],
   theme: {
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
