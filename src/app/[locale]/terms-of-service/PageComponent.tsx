edf232cd9ed2d0e6795b3ac184752d3a042e860f
nivance <limingjian110@163.com>
2024-02-24 17:59:08 +0800

    修改显示问题

--- src/app/[locale]/terms-of-service/PageComponent.tsx
+++ src/app/[locale]/terms-of-service/PageComponent.tsx
@@ -21,7 +21,7 @@ const PageComponent = ({
         page={'terms-of-service'}
         indexLanguageText={indexLanguageText}
       />
-      <main className="w-[95%] md:w-[65%] lg:w-[55%] 2xl:w-[45%] mx-auto h-full my-8">
+      <main className="w-[95%] md:w-[65%] lg:w-[55%] 2xl:w-[45%] mx-auto">
         <div className="p-6 prose mx-auto my-auto text-gray-300">
           <h1 className="text-3xl font-extrabold pb-6 text-white">
             {data('h1')}
